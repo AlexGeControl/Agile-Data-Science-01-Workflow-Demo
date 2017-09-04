e0ba93cd0b11b9c9b3520f27bb0d8dda70b5bd1b
alexge <alexgecontrol@qq.com>
2017-09-04 21:01:08 +0800

    Finish pagination.

--- 01-collect-and-display-records/web/on_time_app_template.py
+++ 01-collect-and-display-records/web/on_time_app_template.py
@@ -6,9 +6,29 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 client = MongoClient()
 
+RECORDS_PER_PAGE = 20
+
+def get_navigation_offsets(start, end, increment):
+    offsets = {}
+    offsets['Previous'] = {
+        'start': max(0, start - increment),
+        'end': max(0, end - increment)
+    }
+    offsets['Next'] = {
+        'start': start + increment,
+        'end':   end + increment
+    }
+
+    return offsets
+
 # Controller: Fetch all flights between cities on a given day and display them
 @app.route("/flights/<origin>/<dest>/<flight_date>")
 def list_flights(origin, dest, flight_date):
+    # Parse pagination params:
+    start = int(request.args.get("start")) or 0
+    end = int(request.args.get("end")) or RECORDS_PER_PAGE
+    num_records = end - start
+
     # Get matching flights:
     flights = client.agile_data_science.on_time_performance.find(
         {
@@ -24,12 +44,16 @@ def list_flights(origin, dest, flight_date):
     # Total number of results:
     flight_count = flights.count()
 
+    flights.skip(start).limit(num_records)
+
     # Render template:
     return render_template(
         'flights.html',
         flights=flights,
         flight_date=flight_date,
-        flight_count=flight_count
+        flight_count=flight_count,
+        nav_path = request.path,
+        nav_offsets = get_navigation_offsets(start, end, RECORDS_PER_PAGE)
     )
 
 # Controller: Fetch one flight record and display
